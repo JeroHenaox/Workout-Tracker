9ecc5d8833d63795408d9f9a29b8418ee46e863a
JeroHenaox <jeroxyt62@gmail.com>
2026-09-14 16:05:20 -0500

    Configuracion de rutas versionadas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const app = express();
 const { port } = require("./config/env");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hola mi server en Express");
 });
+
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
